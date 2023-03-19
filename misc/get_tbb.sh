2fbbb2b20f05489d51e35d4c46767b833e572f69
bo1929 <asapci@ucsd.edu>
2023-03-18 20:57:00 -0700

    Bug fix in get_tbb.sh script.

--- misc/get_tbb.sh
+++ misc/get_tbb.sh
@@ -1,4 +1,5 @@
 wget https://github.com/oneapi-src/oneTBB/archive/2019_U9.tar.gz
 tar -xvzf 2019_U9.tar.gz && rm 2019_U9.tar.gz
-mv -vn oneTBB-2019_U9 "$(dirname -- "$0";)/lib/tbb"
-make -C "$(dirname -- "$0";)/lib/tbb"
+mkdir -p $(dirname -- "$0";)"/../lib"
+mv -vn oneTBB-2019_U9 $(dirname -- "$0";)"/../lib/tbb"
+make -C "$(dirname -- "$0";)/../lib/tbb"
